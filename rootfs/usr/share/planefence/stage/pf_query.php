e5dc2bb94c2e9f499a599a4c839dfd9b5db120f4
soend <priit.heinsaar@gmail.com>
2022-07-18 17:16:49 +0300

    Add "top" argument to PF query interface.

--- rootfs/usr/share/planefence/stage/pf_query.php
+++ rootfs/usr/share/planefence/stage/pf_query.php
@@ -1,4 +1,8 @@
 <?php
+  if(isset($_GET['top'])) {
+    $top = "top=" . $_GET['top'];
+  } else { $top = ""; }
+
   if(isset($_GET['hex'])) {
     $hex = "hex=" . $_GET['hex'];
   } else { $hex = ""; }
@@ -19,9 +23,9 @@
     $outputtype = "type=" . $_GET['type'];
   } else { $outputtype = ""; }
 
-  if (strcmp($hex . $call . $start . $end , "") == 0) {
+  if (strcmp($top . $hex . $call . $start . $end , "") == 0) {
 	   echo "<html><body><H1>PlaneFence Query Interface</H1>";
-	   echo "<h3>Usage: http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] . "?hex=regex&call=regex&start=regex&end=regex&type=csv|json</h3>";
+	   echo "<h3>Usage: http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] . "?top=int&hex=regex&call=regex&start=regex&end=regex&type=csv|json</h3>";
 	   echo "This will read the PlaneFence database and return matching records in JSON format.<br />";
 	   echo "<br />";
 	   echo "At least one argument of hex, call, start, end must be present.<br />";
@@ -30,8 +34,8 @@
 	   echo "The optional type argument indicates if the data returned will be JSON (default if omitted) or CSV.<br />";
 	   echo "<br />";
 	   echo "For example:<br />";
-	   echo "<b>http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] . "?hex=^A[DE]&start=2021/12/1[345]</b><br />";
-	   echo "will return records of which the Hex ID starts with A followed by a D or E, and that have a start date/time that contains 2021/12/13, 2021/12/14, or 2021/12/15.<br />";
+	   echo "<b>http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] . "?top=5&hex=^A[DE]&start=2021/12/1[345]</b><br />";
+	   echo "will return top 5 records of which the Hex ID starts with A followed by a D or E, and that have a start date/time that contains 2021/12/13, 2021/12/14, or 2021/12/15.<br />";
 	   echo "<br />";
 	   echo "Note that the date range is limited to the data available to PlaneFence. By default, this is set to the last 14 days.<hr />";
 	   echo "(C)opyright 2021 by kx1t, available under GPL3 as defined at <a href=https://github.com/kx1t/docker-planefence>the PlaneFence repository at GitHub</a>.<br />";
@@ -42,7 +46,7 @@
      } else {
         header('Content-Type: application/json');
      }
-     system("/usr/share/planefence/pf_query.sh " . escapeshellarg($hex) . " " . escapeshellarg($call) . " " . escapeshellarg($start) . " " . escapeshellarg($end) . " " . escapeshellarg($outputtype) . " file=/usr/share/planefence/html/*.csv", $return_value );
+     system("/usr/share/planefence/pf_query.sh " . escapeshellarg($top) . " " . escapeshellarg($hex) . " " . escapeshellarg($call) . " " . escapeshellarg($start) . " " . escapeshellarg($end) . " " . escapeshellarg($outputtype) . " file=/usr/share/planefence/html/*.csv", $return_value );
      ($return_value == 0) or die("#php error returned an error: $return_value");
   }
 ?>
