ada9decfb1a629f341fb46055e53a02ea5067cd8
soend <priit.heinsaar@gmail.com>
2022-07-30 17:48:12 +0300

    Make everything more responsive and usable on smaller screens.

--- rootfs/usr/share/planefence/stage/v2/components/header.js
+++ rootfs/usr/share/planefence/stage/v2/components/header.js
@@ -5,21 +5,89 @@ export class Header extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-        <nav id="header" class="navbar px-3" role="navigation" aria-label="main navigation">
-            <div class="navbar-brand">
-            <span class="has-text-weight-medium is-size-3">PlaneFence - <data id="station-name"></data></span>
+        <nav id="header" class="navbar" role="navigation" aria-label="main navigation">
+          <div class="navbar-brand">
+            <span class="navbar-item has-text-weight-medium is-size-3-desktop is-size-4-touch">PlaneFence - <data id="station-name"></data></span>
+
+            <a class="navbar-item is-white is-hidden-desktop" target="_blank" href="https://github.com/kx1t/docker-planefence">
+                <span class="icon is-size-3-desktop is-size-4-touch">
+                  <i class="fa-brands fa-github"></i>
+                </span>
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div class="navbar-menu">
+            <div class="navbar-start is-hidden-desktop">
+                <a class="navbar-item" href="index.html">
+                    <span class="icon-text">
+                        <span class="icon">
+                            <i class="fas fa-home"></i>
+                        </span>
+                        <span>Dashboard</span>
+                    </span>
+                </a>
+                <a class="navbar-item" href="./plane-log/">
+                  <span class="icon-text">
+                      <span class="icon">
+                          <i class="fa-solid fa-list"></i>
+                      </span>
+                      <span>24hr Plane log</span>
+                  </span>
+                </a>
+                <a class="navbar-item">
+                  <span class="icon-text">
+                      <span class="icon">
+                          <i class="fa-solid fa-bell"></i>
+                      </span>
+                      <span>Plane alerts</span>
+                  </span>
+                </a>
+                <a class="navbar-item">
+                  <span class="icon-text">
+                      <span class="icon">
+                          <i class="fa-solid fa-clock-rotate-left"></i>
+                      </span>
+                      <span>History</span>
+                  </span>
+                </a>
+                <a class="navbar-item" href="./about/">
+                  <span class="icon-text">
+                      <span class="icon">
+                          <i class="fa-solid fa-circle-info"></i>
+                      </span>
+                      <span>About</span>
+                  </span>
+                </a>
             </div>
 
-            <div class="navbar-end">
-            <button class="button is-white navbar-item" href="https://github.com/kx1t/docker-planefence">
-                <span class="icon is-size-3">
-                <i class="fa-brands fa-github"></i>
-                </span>
-            </button>
+            <div class="navbar-end is-hidden-touch">
+              <a class="navbar-item is-white" target="_blank" href="https://github.com/kx1t/docker-planefence">
+                  <span class="icon is-size-3-desktop is-size-4-touch">
+                    <i class="fa-brands fa-github"></i>
+                  </span>
+              </a>
             </div>
+          </div>
         </nav>
       `;
   }
 }
 
 customElements.define("header-component", Header);
+
+$(document).ready(function() {
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+});
--- rootfs/usr/share/planefence/stage/v2/dashboard.js
+++ rootfs/usr/share/planefence/stage/v2/dashboard.js
@@ -109,10 +109,7 @@ function loadRecentPlaneAndHeatmapData(init) {
   const dateSring = d.getFullYear().toString().slice(-2) + (d.getMonth() + 1).toString().padStart(2, '0') + d.getDate().toString().padStart(2, '0');
 
   $.getScript("../planeheatdata-" + dateSring + ".js").done(function(data) {
-    if (init) {
-      initMap(data);
-    }
-    else {
+    if (!init) {
       updateMap();
     }
   }).fail(function() {
@@ -136,5 +133,6 @@ function loadRecentPlaneAndHeatmapData(init) {
 $(window).on('load', function() { 
   loadData(true);
 
+  initMap();
   initCards();
  });
