939e7a351332aab7dfc07461a0d0b16a745df0ad
soend <priit.heinsaar@gmail.com>
2022-08-02 18:00:25 +0300

    Fix heatmap init

--- rootfs/usr/share/planefence/stage/v2/dashboard.js
+++ rootfs/usr/share/planefence/stage/v2/dashboard.js
@@ -109,9 +109,10 @@ function loadRecentPlaneAndHeatmapData(init) {
   const dateSring = d.getFullYear().toString().slice(-2) + (d.getMonth() + 1).toString().padStart(2, '0') + d.getDate().toString().padStart(2, '0');
 
   $.getScript("../planeheatdata-" + dateSring + ".js").done(function(data) {
-    if (!init) {
-      updateMap();
+    if (init) {
+      initMap();
     }
+    updateMap();
   }).fail(function() {
     console.error("Failed to load heatmap data!");
   });
@@ -133,6 +134,5 @@ function loadRecentPlaneAndHeatmapData(init) {
 $(window).on('load', function() { 
   loadData(true);
 
-  initMap();
   initCards();
  });
