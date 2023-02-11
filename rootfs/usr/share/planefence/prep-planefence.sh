80872c39fe13e1bdf4df1baa5395e138540ffd45
soend <priit.heinsaar@gmail.com>
2023-02-11 11:41:14 +0200

    Add some missing file coping commands.

--- rootfs/usr/share/planefence/prep-planefence.sh
+++ rootfs/usr/share/planefence/prep-planefence.sh
@@ -81,9 +81,13 @@ then
 	cp -f /usr/share/planefence/stage/pa_query.php /usr/share/planefence/html
 	cp -f /usr/share/planefence/stage/pf_query.php /usr/share/planefence/html
 	cp -f /usr/share/planefence/stage/attribution.txt /usr/share/planefence/html
+	cp -f /usr/share/planefence/stage/OpenSkyDb-* /usr/share/planefence/html
 	# unpack operator falgs
 	mkdir -p /usr/share/planefence/html/operatorflags
 	unzip -qq -o -d /usr/share/planefence/html/operatorflags /usr/share/planefence/stage/OperatorFlags.zip
+	# copy alert templates
+	cp -f /usr/share/planefence/stage/plane-alert-discord.template /usr/share/planefence/html
+	cp -f /usr/share/planefence/stage/planefence-discord.template /usr/share/planefence/html
 else
 	cp -f /usr/share/planefence/stage/* /usr/share/planefence/html
 	rm -f /usr/share/planefence/html/planefence.config usr/share/planefence/html/*.template
